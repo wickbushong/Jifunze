1d15d133ed5eddb4f72f0ae28d48db59637b3594
wickbushong <wickbushong@gmail.com>
2020-09-25 11:28:14 -0500

    Specify class_name for Lesson association on User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :lesson_students, foreign_key: :lesson_student_id
     has_many :taken_lessons, through: :lesson_students
-    has_many :given_lessons, foreign_key: :instructor_id
+    has_many :given_lessons, foreign_key: :instructor_id, class_name: "Lesson"
 end
