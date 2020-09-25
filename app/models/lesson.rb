9e1c6f6c72dc1993c8065ca6244b4926538f357b
wickbushong <wickbushong@gmail.com>
2020-09-25 11:23:51 -0500

    Sketch out Lesson model associations.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,5 @@
 class Lesson < ApplicationRecord
-    
+    has_many :lesson_students, foreign_key: :taken_lesson_id
+    has_many :students, through: :lesson_students
+    belongs_to :instructor, class_name: "User"
 end
