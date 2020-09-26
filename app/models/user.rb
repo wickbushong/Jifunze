822b37a5bc31359659fb3e83d176a51b210912fa
wickbushong <wickbushong@gmail.com>
2020-09-26 15:32:07 -0500

    Add secure password to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     # has_many :lesson_students, foreign_key: :lesson_student_id
     # has_many :taken_lessons, through: :lesson_students
     # has_many :given_lessons, foreign_key: :instructor_id, class_name: "Lesson"
