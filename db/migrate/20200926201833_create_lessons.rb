792553dab40a116e92ab791dc426093742cdc0d3
wickbushong <wickbushong@gmail.com>
2020-09-26 15:29:30 -0500

    Generate Lesson migration.

---
+++ db/migrate/20200926201833_create_lessons.rb
@@ -0,0 +1,16 @@
+class CreateLessons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :lessons do |t|
+      t.integer :instructor_id, foreign_key: true
+      t.integer :student_id, foreign_key: true
+      t.datetime :time
+      t.integer :duration
+      t.string :subject
+      t.boolean :booked, default: false
+      t.string :location
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
