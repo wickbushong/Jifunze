cb1689f55b4e1f549a7847b590f9335b947e2352
wickbushong <wickbushong@gmail.com>
2020-09-26 15:43:20 -0500

    Validate presence and uniqueness of User name and email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
     # has_many :lesson_students, foreign_key: :lesson_student_id
     # has_many :taken_lessons, through: :lesson_students
     # has_many :given_lessons, foreign_key: :instructor_id, class_name: "Lesson"
