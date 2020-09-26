edb444d68853198d1c72c2baef703e9b6ad41128
wickbushong <wickbushong@gmail.com>
2020-09-26 14:39:17 -0500

    Comment User assocaitions.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ApplicationRecord
     # has_many :taken_lessons, through: :lesson_students
     # has_many :given_lessons, foreign_key: :instructor_id, class_name: "Lesson"
 
+    # instructor associations
     has_many :given_lessons, foreign_key: "instructor_id", class_name: "Lesson"
-    has_many :taken_lessons, foreign_key: "student_id", class_name: "Lesson"
     has_many :students, through: :given_lessons
+
+    # student associations
+    has_many :taken_lessons, foreign_key: "student_id", class_name: "Lesson"
     has_many :instructors, through: :taken_lessons
 
 end
