a1f522600fe43a52603740f2ea05476500a55b19
wickbushong <wickbushong@gmail.com>
2020-09-28 10:12:24 -0500

    Define strong params for User.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,10 @@ class UsersController < ApplicationController
     def create
         binding.pry
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :email, :instructor)
+    end
 end
