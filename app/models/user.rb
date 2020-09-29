bb18668f780a2e18deacdaba4068eac4f41824c2
wickbushong <wickbushong@gmail.com>
2020-09-29 14:40:12 -0500

    Create lesson show and new views. Add :instructors scope method to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     validates :name, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
+    scope :instructors, -> { where("instructor == true") }
+
     # has_many :lesson_students, foreign_key: :lesson_student_id
     # has_many :taken_lessons, through: :lesson_students
     # has_many :given_lessons, foreign_key: :instructor_id, class_name: "Lesson"
