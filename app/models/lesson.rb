a6aa581f4aae66c67c7ec745335d1d81e92dd33d
wickbushong <wickbushong@gmail.com>
2020-09-29 15:02:31 -0500

    Define :available scope method for Lessons.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,7 @@ class Lesson < ApplicationRecord
     # has_many :lesson_students, foreign_key: :taken_lesson_id
     # has_many :students, through: :lesson_students
     # belongs_to :instructor, class_name: "User"
+    scope :available, -> { where("booked == false") }
 
     belongs_to :instructor, class_name: "User"
     belongs_to :student, class_name: "User"
