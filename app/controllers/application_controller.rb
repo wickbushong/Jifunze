1bdb8ec9b26de2450bed937a37cec3c92755ad25
wickbushong <wickbushong@gmail.com>
2020-09-30 14:07:49 -0500

    Further implement bootstrap styles for new lesson form.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     # before_action :verified_user
     helper_method :current_user
+    helper_method :user_is_authenticated
 
     private
 
