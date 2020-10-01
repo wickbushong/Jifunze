da4170e90c2ba1fbc986c71fee378c9b61600f65
wickbushong <wickbushong@gmail.com>
2020-10-01 12:26:35 -0500

    Validate presence of subject, duration, time, location for new lessons. Display error messages in new lesson view.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,10 @@ class Lesson < ApplicationRecord
     # has_many :lesson_students, foreign_key: :taken_lesson_id
     # has_many :students, through: :lesson_students
     # belongs_to :instructor, class_name: "User"
+    validates :subject, presence: true
+    validates :location, presence: true
+    validates :time, presence: true
+    validates :duration, presence: true
     scope :available, -> { where("booked == false") }
 
     belongs_to :instructor, class_name: "User"
