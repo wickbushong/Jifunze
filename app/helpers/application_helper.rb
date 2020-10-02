d0ad192b47694ab2744ea5a84cb2a8b9e4e485d3
wickbushong <wickbushong@gmail.com>
2020-10-02 16:09:47 -0500

    Build instructor show page to show links to student pages and status of lessons.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,5 +22,13 @@ module ApplicationHelper
             "available"
         end
     end
+
+    def name_link_or_nil(student)
+        if student
+            link_to student.name, user_path(student)
+        else
+            "none (yet)"
+        end
+    end
     
 end
